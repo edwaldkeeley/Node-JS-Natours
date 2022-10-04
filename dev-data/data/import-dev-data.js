c1e0455b06651d6464736db61d43ceabac5df2dd
edwaldkeeley <edwaldpk@gmail.com>
2022-10-04 19:41:35 +0700

    150, GeoSpatial Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,7 +19,7 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 
 const importData = async () => {
