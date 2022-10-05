ae482feb0f9c0084c0947ef09461f92dc75cc8cc
edwaldkeeley <edwaldpk@gmail.com>
2022-10-05 13:27:51 +0700

    151, Modelling Tour Guide

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 const tourSchema = new mongoose.Schema(
   {
@@ -100,7 +101,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +116,13 @@ tourSchema.virtual('durationWeeks').get(function() {
 
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 
