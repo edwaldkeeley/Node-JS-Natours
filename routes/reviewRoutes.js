96d4920b57451f14433e0f9e0588db6b340e6d4a
Edwald <91954979+edwaldkeeley@users.noreply.github.com>
2022-12-10 23:04:04 +0700

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -7,8 +7,6 @@ const router = express.Router({ mergeParams: true });
 // Protect All Routes below it
 router.use(authController.protect);
 
-// for commit
-
 router
   .route('/')
   // Get All review
