ee0eb3ed5a61283ee31ecbe8fe21a7534d340472
Edwald <91954979+edwaldkeeley@users.noreply.github.com>
2022-12-30 23:15:38 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,6 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
 
-//commit
-
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
